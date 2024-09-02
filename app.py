77f0d72a34fd6d9d7c00dd49a51b872942844191
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-09-02 17:25:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ pdf_file = "tarea-6.pdf"
 with open(pdf_file, "rb") as f:
     pdf_data = f.read()
     st.download_button(
-        label="Descargar PDF",
+        label="Descargar manual de tarea-6 en PDF",
         data=pdf_data,
         file_name="tarea-6.pdf",
         mime="application/pdf"
